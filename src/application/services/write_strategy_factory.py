0c464a52504f77e755112574f025402268a6154d
Yuliia Antonova <yuliiaantonova@macbookpro.localdomain>
2026-03-22 23:15:24 +0100

    update code with useless class

--- src/application/services/write_strategy_factory.py
+++ src/application/services/write_strategy_factory.py
@@ -2,7 +2,7 @@ from src.domain.contracts.write_strategy import BaseWriteStrategy
 from src.infrastructure.writing.append_writer import AppendWriteStrategy
 from src.infrastructure.writing.replace_writer import ReplaceWriteStrategy
 from src.infrastructure.writing.upsert_writer import UpsertWriteStrategy
-from src.infrastructure.writing.versioned_writer import VersionedWriteStrategy
+# from src.infrastructure.writing.versioned_writer import VersionedWriteStrategy
 
 
 WRITE_MODES = ["replace", "append", "upsert", "versioned"]
@@ -25,5 +25,5 @@ def get_write_strategy(
     if write_mode == "versioned":
         if not primary_key:
             raise ValueError("primary_key is required for versioned write mode")
-        return VersionedWriteStrategy(connector=connector, table_name=table_name, primary_key=primary_key)
+        # return VersionedWriteStrategy(connector=connector, table_name=table_name, primary_key=primary_key)
     raise ValueError(f"Unsupported write_mode: {write_mode}")
